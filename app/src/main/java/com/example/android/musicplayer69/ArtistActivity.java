d971ee061b81f305cbc2c2351e9fda2d3f504ebe
barbozzzza <38773791+barbozzzza@users.noreply.github.com>
2018-11-01 14:12:45 -0500

    Music Player app for udacity Musical Structure app.

--- app/src/main/java/com/example/android/musicplayer69/ArtistActivity.java
+++ app/src/main/java/com/example/android/musicplayer69/ArtistActivity.java
@@ -1,20 +1,27 @@
 package com.example.android.musicplayer69;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 
 public class ArtistActivity extends AppCompatActivity {
     public static final String KEY_SONG = "KEY_SONG";
     public static final String KEY_ARTIST = "KEY_ARTIST";
-    public static final String KEY_ALBUM = "KEY_ALBUM";
-    public static Integer Key_AlbumArt;
+
+
+    // I am not certain how to declare this variable in order to be able to call it in the other activity.
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +34,14 @@ public class ArtistActivity extends AppCompatActivity {
         /** words.add("one")*/
 
         song.add(new Song("Last Night", "The Strokes", "Is this It",R.drawable.albumcover));
-        song.add(new Song("Golden Fleeces", "Israel Nash", "Lifted",R.drawable.play));
-        song.add(new Song("Heavy, California", "Jungle", "Heavy California",R.drawable.albumcover));
-        song.add(new Song("Grow Up", "Boiler", "Grow Up",R.drawable.albumcover));
-        song.add(new Song("The Ballad of the Costa Concordia", "Car Seat Head Rest", "Teens of Denial",R.drawable.albumcover));
-        song.add(new Song("Leaving", "Bass Drum of Death", "Just Business",R.drawable.albumcover));
-        song.add(new Song("Creature Comfort", "Woods", "City Sun Eater in the River of Light",R.drawable.albumcover));
-        song.add(new Song("Summer with Phill", "Dead Ghost", "Can't Get No",R.drawable.albumcover));
-        song.add(new Song("Night Ride", "The Growlers", "Night Ride",R.drawable.albumcover));
+        song.add(new Song("Golden Fleeces", "Israel Nash", "Lifted",R.drawable.goldenfleece));
+        song.add(new Song("Heavy, California", "Jungle", "Heavy California",R.drawable.heavycalifornia));
+        song.add(new Song("Grow Up", "Boiler", "Grow Up",R.drawable.boiler));
+        song.add(new Song("The Ballad of the Costa Concordia", "Car Seat Head Rest", "Teens of Denial",R.drawable.thebaladofthecostaconcordia));
+        song.add(new Song("Leaving", "Bass Drum of Death", "Just Business",R.drawable.justbusiness));
+        song.add(new Song("Creature Comfort", "Woods", "City Sun Eater in the River of Light",R.drawable.theriveroflight));
+        song.add(new Song("Summer with Phill", "Dead Ghost", "Can't Get No",R.drawable.cantgetno));
+        song.add(new Song("Night Ride", "The Growlers", "Night Ride",R.drawable.nightride));
 
 
         final SongAdapter adapter = new SongAdapter(this, song);
@@ -49,16 +56,34 @@ public class ArtistActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Song item = adapter.getItem(position);
-                Intent nowPlaying = new Intent(getApplicationContext(), MainActivity.class);
-                nowPlaying.putExtra(KEY_SONG, item.getmSong());
-                nowPlaying.putExtra(KEY_ALBUM, item.getAlbums());
-                nowPlaying.putExtra(KEY_ARTIST, item.getmArtist());
-                nowPlaying.putExtra(String.valueOf(Key_AlbumArt),item.getAlbumArt());
-                startActivity(nowPlaying);
-            }
+                Intent intent = new Intent(ArtistActivity.this, MainActivity.class);
+                intent.putExtra(KEY_SONG, item.getmSong());
+                intent.putExtra(KEY_ARTIST, item.getmArtist());
+                intent.putExtra("ALBUM_ART",item.getAlbumArt());
+                startActivity(intent);}
+
+
+
+
+
 
         });
 
 
+
     }
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/android/musicplayer69/MainActivity.java
+++ app/src/main/java/com/example/android/musicplayer69/MainActivity.java
@@ -1,56 +1,52 @@
 package com.example.android.musicplayer69;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import static com.example.android.musicplayer69.ArtistActivity.KEY_ARTIST;
 import static com.example.android.musicplayer69.ArtistActivity.KEY_SONG;
-import static com.example.android.musicplayer69.ArtistActivity.Key_AlbumArt;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-
-        String playingSong = "";
-        String playingArtist = "";
-        String nowPlayingSong = "";
-
-
-
-
         Intent intent = getIntent();
-        if (null != intent) {
-            playingSong = intent.getStringExtra(KEY_SONG);
-            playingArtist = intent.getStringExtra(KEY_ARTIST);
-            nowPlayingSong = intent.getIntegerArrayListExtra(Key_AlbumArt);
-            {
-            }
-            
-             {
-            }
+
+        String PlayingSong = intent.getStringExtra(KEY_SONG);
+        String playingArtist = intent.getStringExtra(KEY_ARTIST);
+        int albumArt = intent.getIntExtra("ALBUM_ART",0);
 
 
-        }
 
-// Sets data to appropriate TextViews
-        TextView playingSongText = (TextView) findViewById(R.id.Song);
-        playingSongText.setText(playingSong);
+        TextView currentSong = findViewById(R.id.Song);
+        currentSong.setText(PlayingSong);
 
+        TextView currentArtist = findViewById(R.id.Artist);
+        currentArtist.setText(playingArtist);
 
-        TextView playingArtistText = (TextView) findViewById(R.id.Artist);
-        playingArtistText.setText(playingArtist);
+        ImageView currentAlbumArt = findViewById(R.id.AlbumCover);
+        currentAlbumArt.setImageResource(albumArt);
 
-        ImageView CurrentAlbumArt = findViewById(R.id.AlbumCover);
-        CurrentAlbumArt.setImageResource(Integer.parseInt(nowPlayingSong));
-
+        ImageView currentArtistSmallArt = findViewById(R.id.SmallAlbumArt);
+        currentArtistSmallArt.setImageResource(albumArt);
 
 
 
@@ -61,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
         menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent menuIntent = new Intent(MainActivity.this, menuActivity.class);
+                Intent menuIntent = new Intent(MainActivity.this, ArtistActivity.class);
                 startActivity(menuIntent);
             }
         });
